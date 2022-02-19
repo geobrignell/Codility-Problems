e56394d0bb6f2042272ad771022056ad6e8a9d7a
George Brignell-Cash <georgebrignellcash@gmail.com>
2022-02-19 15:48:39 +0000

    First Upload

---
+++ binarygap.py
@@ -0,0 +1,14 @@
+#%%
+"""
+N is an integer number.
+Converted into binarystring using bin()
+Leading and Tailing 0s removed using strip()
+String split into groups of 0s using split()
+New array of sizes of 0 groups
+Returned maximum size of group
+"""
+def solution(N):
+    binary = bin(N)[2:].strip("0").split("1")
+    size = [len(i) for i in binary]
+    return max(size)
+# %%
